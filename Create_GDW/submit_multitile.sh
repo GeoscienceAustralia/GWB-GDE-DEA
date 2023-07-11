dc302369f43b20e25db48aea55a42dc2693a27a4
pkilgour <penny.kilgour@ga.gov.au>
2023-07-11 18:59:09 +1000

    Update submit_multitile.sh

--- Create_GDW/submit_multitile.sh
+++ Create_GDW/submit_multitile.sh
@@ -19,4 +19,5 @@
 module use /g/data/v10/public/modules/modulefiles
 module load dea
 
+#run python script to extract Water Observation statistics by tiles, for those waterbodies that cover more than one tile
 python extractWOFS_by_multitile.py
